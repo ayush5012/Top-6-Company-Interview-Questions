eca7ef96ee72fc0a617d1c62c6d7190cea369c69
Ayush yadav <yayush2002@gmail.com>
2022-01-13 14:19:37 +0530

    GS section started

---
+++ Goldman Sachs/Print Anagrams together.cpp
@@ -0,0 +1,65 @@
+// Question link: https://practice.geeksforgeeks.org/problems/print-anagrams-together/1/#
+
+//CODE
+
+// { Driver Code Starts
+//Initial Template for C++
+#include <bits/stdc++.h>
+#include <unordered_map>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+  public:
+    vector<vector<string>> Anagrams(vector<string>& s) {
+        unordered_map<string, vector<string>> mp;
+        for(auto &x: s){
+            string a = x;
+            sort(a.begin(), a.end());
+            mp[a].push_back(x);
+        }
+        
+        vector<vector<string>> ans;
+        for(auto &x: mp){
+            vector<string> temp;
+            for(auto &y: x.second){
+                temp.push_back(y);
+            }
+            ans.push_back(temp);
+        }
+        return ans;
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        vector<string> string_list(n);
+        for (int i = 0; i < n; ++i)
+            cin>>string_list[i]; 
+        Solution ob;
+        vector<vector<string> > result = ob.Anagrams(string_list);
+        sort(result.begin(),result.end());
+        for (int i = 0; i < result.size(); i++)
+        {
+            for(int j=0; j < result[i].size(); j++)
+            {
+                cout<<result[i][j]<<" ";
+            }
+            cout<<"\n";
+        }
+    }
+
+    return 0;
+}
+  // } Driver Code Ends
